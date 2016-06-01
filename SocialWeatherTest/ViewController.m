d72bbd9bbfb48df12437e0feb248ca329092c7f3
Robert de Vries <r.devries@robbytu.net>
2016-06-02 01:00:40 +0200

    + Added the resources and actual views

--- SocialWeatherTest/ViewController.m
+++ SocialWeatherTest/ViewController.m
@@ -10,18 +10,34 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *blurContainer;
+@property (weak, nonatomic) IBOutlet UIView *userDetailView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setupScrollView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupScrollView {
+    CGSize baseFrame = self.view.frame.size;
+    CGSize detailFrame = self.userDetailView.frame.size;
+    
+    [self.scrollView setContentSize: CGSizeMake(baseFrame.width, baseFrame.height * 2 - detailFrame.height)];
+    [self.scrollView setPagingEnabled: YES];
+    [self.scrollView setDelegate: self];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGPoint offset = [scrollView contentOffset];
+    
+    // Update alpha of the blurry layer
+    float alpha = MIN(offset.y / 100.0, 1.0);
+    [self.blurContainer setAlpha: alpha];
 }
 
 @end
