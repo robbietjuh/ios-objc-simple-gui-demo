177e8d28e4e5b2bc65a3949aa2cac676109ccfe2
Robert de Vries <r.devries@robbytu.net>
2016-06-09 19:14:44 +0200

    + Implemented the GUI for actually uploading a picture

--- SocialWeatherTest/CameraViewController.swift
+++ SocialWeatherTest/CameraViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import AVFoundation
 import CoreLocation
+import M13Checkbox
 
 class CameraViewController : DismissableViewController, CLLocationManagerDelegate {
     
@@ -18,15 +19,18 @@ class CameraViewController : DismissableViewController, CLLocationManagerDelegat
     
     @IBOutlet weak var weatherScrollView: UIScrollView!
     @IBOutlet weak var labelTemperature: UILabel!
+    @IBOutlet weak var labelInstruction: UILabel!
     @IBOutlet weak var labelConditionsSunny: UILabel!
     @IBOutlet weak var labelConditionsCloudy: UILabel!
     @IBOutlet weak var labelConditionsRainy: UILabel!
     @IBOutlet weak var labelConditionsFoggy: UILabel!
     @IBOutlet weak var labelConditionsThunder: UILabel!
     
+    @IBOutlet weak var loadingView: UIView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     // Initialize a location manager for GPS coords
     let locationManager = CLLocationManager()
-    
     
     // Set up a new AVCaptureSession instance
     let session = AVCaptureSession()
@@ -44,6 +48,8 @@ class CameraViewController : DismissableViewController, CLLocationManagerDelegat
     var currentInput : AVCaptureInput?
     var currentPreview = AVCaptureVideoPreviewLayer()
     var currentStep = 0
+    var location : CLLocationCoordinate2D?
+    var imageData : NSData?
     let shutter = AVCaptureStillImageOutput()
     
     // MARK: - View setup
@@ -83,7 +89,7 @@ class CameraViewController : DismissableViewController, CLLocationManagerDelegat
     
     override func viewDidAppear(animated: Bool) {
         // We'll have to take care of privacy settings, so let's check wether any devices were found
-        guard self.devices.count == 0 else {
+        guard self.devices.count > 0 else {
             return self.dismissViewControllerWithMessage("No camera found", message: "We could not get access to a camera. Please check your privacy settings in the Settings app.")
         }
         
@@ -123,23 +129,36 @@ class CameraViewController : DismissableViewController, CLLocationManagerDelegat
         // Stop updating the location
         self.locationManager.stopUpdatingLocation()
         
-        // Get the current weather
-        OWMApiClient.currentWeatherByCoordinates(newLocation.coordinate, success: { (city, temp) in
-            // Put the city and weather details into the views
-            self.labelConditionsSunny.text = "Sunny \(city)".uppercaseString
-            self.labelConditionsCloudy.text = "Cloudy \(city)".uppercaseString
-            self.labelConditionsRainy.text = "Rainy \(city)".uppercaseString
-            self.labelConditionsFoggy.text = "Foggy \(city)".uppercaseString
-            self.labelConditionsThunder.text = "Thundering \(city)".uppercaseString
-            self.labelTemperature.text = "\(temp)°"
-            
-            // Show the views
-            UIView.animateWithDuration(0.6, animations: {
-                self.labelTemperature.alpha = 1
-                self.weatherScrollView.alpha = 1
-            })
-        }) { (errorMessage) in
-            self.dismissViewControllerWithMessage("Location issues", message: "We could not get the weather for your current location. \(errorMessage)")
+        // Save the location
+        self.location = newLocation.coordinate
+        
+        // Check wether we're waiting for the photo to upload
+        if self.currentStep == 2 {
+            // We are. Just call the upload handler, it'll take care of the rest. 
+            // Don't animate any of the labels and stuff in; if the upload process
+            // takes shorter than the animation process, that can potentially result
+            // in weird view behaviours.
+            self.uploadPicture()
+        }
+        else {
+            // Get the current weather
+            OWMApiClient.currentWeatherByCoordinates(self.location!, success: { (city, temp) in
+                // Put the city and weather details into the views
+                self.labelConditionsSunny.text = "Sunny \(city)".uppercaseString
+                self.labelConditionsCloudy.text = "Cloudy \(city)".uppercaseString
+                self.labelConditionsRainy.text = "Rainy \(city)".uppercaseString
+                self.labelConditionsFoggy.text = "Foggy \(city)".uppercaseString
+                self.labelConditionsThunder.text = "Thundering \(city)".uppercaseString
+                self.labelTemperature.text = "\(temp)°"
+                
+                // Show the views
+                UIView.animateWithDuration(0.6, animations: {
+                    self.labelTemperature.alpha = 1
+                    self.weatherScrollView.alpha = 1
+                })
+            }) { (errorMessage) in
+                self.dismissViewControllerWithMessage("Location issues", message: "We could not get the weather for your current location. \(errorMessage)")
+            }
         }
     }
     
@@ -174,27 +193,88 @@ class CameraViewController : DismissableViewController, CLLocationManagerDelegat
     }
     
     func handleTap(action: AnyObject!) {
-        // Make sure to be in the very first step of taking a photo
-        guard self.currentStep == 0 else { return }
-        self.currentStep = 1
+        self.currentStep = self.currentStep + 1
         
-        // Try to safely get the output connection
-        if(self.shutter.connections.count != 1) { return }
-        guard let connection = self.shutter.connections[0] as? AVCaptureConnection else { return }
+        switch self.currentStep {
+        case 1:
+            // Try to safely get the output connection
+            if(self.shutter.connections.count != 1) { return }
+            guard let connection = self.shutter.connections[0] as? AVCaptureConnection else { return }
         
-        // Now capture whatever is on that connection as an image
-        self.shutter.captureStillImageAsynchronouslyFromConnection(connection) { (buffer: CMSampleBuffer!, error: NSError!) in
-            let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(buffer)
-            let image = UIImage(data: imageData)
+            // Now capture whatever is on that connection as an image
+            self.shutter.captureStillImageAsynchronouslyFromConnection(connection) { (buffer: CMSampleBuffer!, error: NSError!) in
+                self.imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(buffer)
+                let image = UIImage(data: self.imageData!)
             
-            // And display it to the user
-            self.picture.image = image
-            self.picture.hidden = false
-        }
+                // And display it to the user
+                self.picture.image = image
+                self.picture.hidden = false
+            }
         
-        // Hide layers that we do not need now
-        self.currentPreview.hidden = true
-        self.switchButton.hidden = true
+            // Hide layers that we do not need now
+            self.currentPreview.hidden = true
+            self.switchButton.hidden = true
+            
+            // Update the instruction text
+            self.labelInstruction.text = "Swipe to customize, tap to upload"
+            
+        case 2:
+            // Animate fade-in for the blur view
+            UIView.animateWithDuration(0.4, animations: {
+                self.loadingView.hidden = false
+                self.loadingView.alpha = 1
+            }, completion: { (Bool) in
+                // We don't want to do the uploading in this function, we want to hand it
+                // off to another function. That is because of the speed of GPS. If GPS is
+                // slow to respond, we want to wait for the GPS handler to complete. If it
+                // completes, it'll call the same function. That way, we don't have to
+                // have redundant pieces of code.
+                self.uploadPicture()
+            })
+            
+        default:
+            self.currentStep = self.currentStep - 1
+            NSLog("CameraViewController is in an unknown state (\(self.currentStep)).")
+        }
+    }
+    
+    func uploadPicture() {
+        // Safely get the variables we need
+        guard let location = self.location,
+              let picture = self.imageData
+              else {
+                  return
+              }
+        
+        let weatherType = self.weatherScrollView.contentOffset.x / self.weatherScrollView.frame.size.width
+        
+        // TODO: Send location, picture and weatherType to the remote api server
+        NSLog("Upload to server")
+        
+        // Show an animated checkbox on completion
+        UIView.animateWithDuration(0.4, animations: { 
+            self.activityIndicator.alpha = 0
+        }) { (Bool) in
+            self.activityIndicator.stopAnimating()
+            
+            let checkbox = M13Checkbox(frame: CGRectMake(self.view.bounds.width / 2 - 60, self.view.bounds.height / 2 - 60, 120, 120))
+            
+            checkbox.tintColor = UIColor.whiteColor()
+            checkbox.secondaryTintColor = UIColor.clearColor()
+            checkbox.animationDuration = 0.4
+            
+            self.loadingView.addSubview(checkbox)
+            
+            checkbox.setCheckState(.Checked, animated: true)
+            
+            // Schedule a timer to finish the upload/animation
+            NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(self.finishUploadPicture), userInfo: 0, repeats: false)
+        }
+    }
+    
+    func finishUploadPicture(timer: NSTimer) {
+        // Dismiss the view
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
 }
