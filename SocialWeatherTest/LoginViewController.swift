f9d654cde57746fde4477b0d04fb803ed0c071d5
Robert de Vries <r.devries@robbytu.net>
2016-06-22 01:30:48 +0200

    * Fixes for registration

--- SocialWeatherTest/LoginViewController.swift
+++ SocialWeatherTest/LoginViewController.swift
@@ -7,64 +7,84 @@
 //
 
 import UIKit
-import Alamofire
 import M13Checkbox
 
-class LoginViewController : UIViewController {
+class LoginViewController : UIViewController, UITextFieldDelegate, UIScrollViewDelegate {
     
     @IBOutlet weak var emailField: UITextField!
-    @IBOutlet weak var errorTextField: UITextField!
-    @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var skipButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     
-    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
-    var container: UIView = UIView()
-    var loadingView: UIView = UIView()
-    
-    //-----------------------------------------------------
-    //DONT FORGET TO FILL IN THE BASE URL!
-    //-----------------------------------------------------
-    var baseUrl: String = ""
-    
-    override func viewDidLoad() {
-        continueButton.enabled = false
-    }
-    
-    @IBAction func emailFieldChanged(sender: AnyObject) {
-        if(self.emailField != ""){
-            continueButton.enabled = true
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        
+        if(!emailTest.evaluateWithObject(textField.text)) {
+            let alert = UIAlertController(title: "Invalid e-mail address",
+                                          message: "Please enter a valid e-mail address to continue.",
+                                          preferredStyle: .Alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+            return false
         }
-    }
-
-    @IBAction func ContinueButtonPressed(sender: AnyObject) {
-        authenticateLogin(emailField.text!)
+        
+        self.authenticateLogin(textField.text!)
+        
+        return false
     }
     
     func authenticateLogin(email:String){
-        if(email != ""){
-            //spinner draaien
-            showActivityIndicator(self.view)
-            
-            //email sturen en kijken of authenticated
-            Alamofire.request(.POST, baseUrl+"/api/user/register", parameters: ["email": email])
-                .responseJSON { _, _, result in
-                    print(result)
-                    debugPrint(result)
+        self.activityIndicator.startAnimating()
+        self.skipButton.hidden = true
+        self.emailField.enabled = false
+        
+        SWApiClient.register(email) { result in
+            switch result {
+            case .Success(let json):
+                let response = json as! NSDictionary
+                if response.objectForKey("success") as! Bool {
+                    self.segueSuccessfulRegistration()
+                }
+                else {
+                    self.activityIndicator.startAnimating()
+                    self.skipButton.hidden = true
+                    self.emailField.enabled = false
+                    
+                    let alert = UIAlertController(title: "Could not process your registration",
+                                                  message: nil,
+                                                  preferredStyle: .Alert)
+                    
+                    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+                break
+                
+            case .Failure(_, _):
+                self.activityIndicator.startAnimating()
+                self.skipButton.hidden = true
+                self.emailField.enabled = false
+                
+                let alert = UIAlertController(title: "Could not connect to the remote server",
+                                              message: nil,
+                                              preferredStyle: .Alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+                
+                break
             }
-            
-            //spinner stoppen
-            hideActivityIndicator(self.view)
-            
-            continueButton.enabled = false
-            
-            requestSentView()
-            //message returnen: wel/niet ingelogd
-        } else {
-            errorTextField.text = "Please fill in a correct e-mail."
         }
     }
     
-    func requestSentView(){
-        let checkbox = M13Checkbox(frame: CGRectMake(self.view.bounds.width / 2 - 60, self.view.bounds.height / 2 - 60, 120, 120))
+    func segueSuccessfulRegistration(){
+        self.scrollView.scrollRectToVisible(CGRectMake(self.scrollView.frame.width, 0, self.scrollView.frame.width, self.scrollView.frame.height), animated: true)
+    }
+    
+    func scrollViewDidEndScrollingAnimation(scrollView: UIScrollView) {
+        let checkbox = M13Checkbox(frame: CGRectMake(self.view.bounds.width / 2 - 60, self.view.bounds.height / 2 - 100 - 60 - 20, 120, 120))
         
         checkbox.tintColor = UIColor.whiteColor()
         checkbox.secondaryTintColor = UIColor.clearColor()
@@ -73,39 +93,5 @@ class LoginViewController : UIViewController {
         self.view.addSubview(checkbox)
         
         checkbox.setCheckState(.Checked, animated: true)
-        
-    }
-    
-    func showActivityIndicator(uiView: UIView) {
-        container.frame = uiView.frame
-        container.center = uiView.center
-        container.backgroundColor = UIColorFromHex(0xffffff, alpha: 0.3)
-        
-        loadingView.frame = CGRectMake(0, 0, 80, 80)
-        loadingView.center = uiView.center
-        loadingView.backgroundColor = UIColorFromHex(0x444444, alpha: 0.7)
-        loadingView.clipsToBounds = true
-        loadingView.layer.cornerRadius = 10
-        
-        activityIndicator.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
-        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.WhiteLarge
-        activityIndicator.center = CGPointMake(loadingView.frame.size.width / 2, loadingView.frame.size.height / 2);
-        
-        loadingView.addSubview(activityIndicator)
-        container.addSubview(loadingView)
-        uiView.addSubview(container)
-        activityIndicator.startAnimating()
-    }
-    
-    func hideActivityIndicator(uiView: UIView) {
-        activityIndicator.stopAnimating()
-        container.removeFromSuperview()
-    }
-    
-    func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
-        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
-        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
-        let blue = CGFloat(rgbValue & 0xFF)/256.0
-        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
     }
 }
---
+++ SocialWeatherTest/SWApiClient.swift
@@ -0,0 +1,22 @@
+//
+//  SWApiClient.swift
+//  SocialWeatherTest
+//
+//  Created by R. de Vries on 21-06-16.
+//  Copyright © 2016 R. de Vries. All rights reserved.
+//
+
+//import Foundation
+import Alamofire
+
+class SWApiClient {
+    static let BASE_URL = "http://intern.robbytu.net:8000/api"
+    
+    static func register(email: String, callback: (Result<AnyObject>) -> (Void)) {
+        Alamofire.request(.POST, BASE_URL + "/user/register", parameters: ["email": email])
+            .responseJSON { _, _, result in
+                callback(result)
+        }
+    }
+}
+
